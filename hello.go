8d4ec8d76ee167b9c3ce6131aacee1812e8bfd57
Sky <18810590611@163.com>
2022-11-30 14:12:27 +0800

    修改 hello.go 中函数的表示，防止过于臃肿，增强可读性

--- hello.go
+++ hello.go
@@ -18,16 +18,19 @@ func Hello(name string, language string) string {
 		name = "World"
 	}
 
-	prefix := englishHelloPrefix
+	return greetingPrefix(language) + name
+}
 
+func greetingPrefix(language string) (prefix string) {
 	switch language {
 	case french:
 		prefix = frenchHelloPrefix
 	case spanish:
 		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-
-	return prefix + name
+	return
 }
 
 func main() {
