be33147abfda3fad316f0a5dcfc8e9547ccbb5de
Sky <18810590611@163.com>
2022-11-30 14:00:53 +0800

    添加了法语模式

--- hello.go
+++ hello.go
@@ -5,13 +5,29 @@ import "fmt"
 // 避免每次创建字符串实例
 const englishHelloPrefix = "Hello, "
 
-func Hello(name string) string {
+// 西班牙语
+const spanish = "Spanish"
+const spanishHelloPrefix = "Hola, "
+
+// 法语
+const french = "French"
+const frenchHelloPrefix = "Bonjour, "
+
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
+	}
+
+	if language == spanish {
+		return spanishHelloPrefix + name
+	}
+
+	if language == french {
+		return frenchHelloPrefix + name
 	}
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
